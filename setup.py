28e971e22a14355d067bfe802d8e65f11ccb95aa
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2021-11-25 12:15:01 +0100

    update setup for icm

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     url="https://github.com/qgallouedec/go-explore",
     description="Implementation of Go-Explore",
     long_description=open("README.md").read(),
-    install_requires=["stable_baselines3 @ git+https://git@github.com/qgallouedec/stable-baselines3.git", "scipy"],
+    install_requires=["stable_baselines3 @ git+https://git@github.com/qgallouedec/stable-baselines3@ICM-compat", "scipy"],
     extras_require={
         "tests": ["pytest", "black"],
         "extra": ["panda-gym", "optuna"],
