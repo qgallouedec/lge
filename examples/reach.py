e09c74eb3301f9055e1404ce853f326b5a21e36f
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2022-03-22 11:49:24 +0100

    rm examples

--- examples/reach.py
+++
@@ -1,9 +0,0 @@
-import gym
-import panda_gym
-from go_explore.go_explore.cell_computers import PandaCellComputer
-
-from go_explore.go_explore.go_explore import GoExplore
-
-env = gym.make("PandaNoTask-v0")
-ge = GoExplore(env, PandaCellComputer(), subgoal_horizon=10, verbose=1)
-ge.exploration(15000, eval_freq=500, n_eval_episodes=50)
