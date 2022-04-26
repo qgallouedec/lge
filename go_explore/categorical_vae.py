cd16457c28f7c40ea4f19df4cb3121f8ba832a7e
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2022-04-26 15:11:38 +0200

    Docstring

--- go_explore/categorical_vae.py
+++ go_explore/categorical_vae.py
@@ -64,11 +64,61 @@ class GaussianVAE(nn.Module):
 
 
 class CategoricalVAE(nn.Module):
-    def __init__(self, nb_classes: int = 32, nb_categoricals: int = 32, in_channels: int = 3, tau: float = 1.0) -> None:
+    """
+    Categorical Variational Auto-Encoder.
+
+    Encoder:
+
+    | Size      | Channels       |
+    |-----------|----------------|
+    | 129 x 129 | input channels |
+    | 129 x 129 | 8              |
+    | 65 x 65   | 16             |
+    | 33 x 33   | 32             |
+    | 17 x 17   | 64             |
+    | 9 x 9     | 128            |
+    | 5 x 5     | 256            |
+    | 3 x 3     | 512            |
+
+    The result is flattened to a vector of size 4608.
+    The result is passed through a fully connected layer that utput size is nb_categoricals x nb_classes vector.
+    The latent is sampled from the Gumbel-Softmax distribution.
+    The result is passed through a fully connected layer that utput size is 4608.
+    The result is unflattened to a vector of size 3 x 3 x 512.
+
+    Decoder:
+
+    | Size      | Channels       |
+    |-----------|----------------|
+    | 3 x 3     | 512            |
+    | 5 x 5     | 256            |
+    | 9 x 9     | 128            |
+    | 17 x 17   | 64             |
+    | 33 x 33   | 32             |
+    | 65 x 65   | 16             |
+    | 129 x 129 | 8              |
+    | 129 x 129 | input channels |
+
+    :param nb_classes: Number of classes per categorical distribution
+    :param nb_categoricals: Number of categorical distributions
+    :param in_channels: Number of input channels
+    :param tau: Temparture in gumbel sampling
+    :param hard_sampling: If True, the latent is sampled will be discretized as one-hot vectors
+    """
+
+    def __init__(
+        self,
+        nb_classes: int = 32,
+        nb_categoricals: int = 32,
+        in_channels: int = 3,
+        tau: float = 1.0,
+        hard_sampling: bool = True,
+    ) -> None:
         super(CategoricalVAE, self).__init__()
         self.nb_classes = nb_classes
         self.nb_categoricals = nb_categoricals
         self.tau = tau
+        self.hard_sampling = hard_sampling
         _h = 8
         self.encoder = nn.Sequential(  # [N x C x 129 x 129]
             nn.Conv2d(in_channels, _h * 1, kernel_size=3, stride=1, padding=1),  # [N x 8 x 129 x 129]
@@ -112,7 +162,7 @@ class CategoricalVAE(nn.Module):
 
     def forward(self, x: Tensor) -> Tuple[Tensor, Tensor, Tensor]:
         logits = self.encoder(x)
-        latent = F.gumbel_softmax(logits, tau=self.tau, hard=True)
+        latent = F.gumbel_softmax(logits, tau=self.tau, hard=self.hard_sampling)
         recons = self.decoder(latent)
         # Compute kl
         probs = F.softmax(logits, dim=2)
