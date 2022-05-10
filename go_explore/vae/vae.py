1a8e4a627538fb0253b55ef4b4050920ca582fba
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2022-05-10 18:42:44 +0200

    recover deepest conv2d

--- go_explore/vae/vae.py
+++ go_explore/vae/vae.py
@@ -117,20 +117,22 @@ class CNNCategoricalVAE(nn.Module):
         self.bn4 = nn.BatchNorm2d(64)
         self.conv5 = nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1)
         self.bn5 = nn.BatchNorm2d(128)
-        # self.conv6 = nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1)
-        self.linear1 = nn.Linear(128 * 9 * 9, self.nb_categoricals * self.nb_classes)
+        self.conv6 = nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1)
+        self.bn6 = nn.BatchNorm2d(256)
+        self.linear1 = nn.Linear(256 * 5 * 5, self.nb_categoricals * self.nb_classes)
 
         # Decoder
-        self.linear2 = nn.Linear(self.nb_categoricals * self.nb_classes, 128 * 9 * 9)
-        # self.tconv1 = nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1)
+        self.linear2 = nn.Linear(self.nb_categoricals * self.nb_classes, 256 * 5 * 5)
+        self.tconv1 = nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1)
+        self.bn7 = nn.BatchNorm2d(128)
         self.tconv2 = nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1)
-        self.bn6 = nn.BatchNorm2d(64)
+        self.bn8 = nn.BatchNorm2d(64)
         self.tconv3 = nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1)
-        self.bn7 = nn.BatchNorm2d(32)
+        self.bn9 = nn.BatchNorm2d(32)
         self.tconv4 = nn.ConvTranspose2d(32, 16, kernel_size=3, stride=2, padding=1)
-        self.bn8 = nn.BatchNorm2d(16)
+        self.bn10 = nn.BatchNorm2d(16)
         self.tconv5 = nn.ConvTranspose2d(16, 8, kernel_size=3, stride=2, padding=1)
-        self.bn9 = nn.BatchNorm2d(8)
+        self.bn11 = nn.BatchNorm2d(8)
         self.tconv6 = nn.ConvTranspose2d(8, in_channels, kernel_size=3, stride=1, padding=1)
 
     def encode(self, x: Tensor) -> Tensor:
@@ -150,8 +152,9 @@ class CNNCategoricalVAE(nn.Module):
         x = self.conv5(x)  # [N x 128 x 9 x 9]
         x = self.bn5(x)
         x = F.relu(x)
-        # x = self.conv6(x)  # [N x 256 x 5 x 5]
-        # x = F.relu(x)
+        x = self.conv6(x)  # [N x 256 x 5 x 5]
+        x = self.bn6(x)
+        x = F.relu(x)
         x = x.flatten(start_dim=1)  # [N x 256*5*5]
         x = self.linear1(x)  # type: Tensor # [N x k*l]
         return x
@@ -159,20 +162,21 @@ class CNNCategoricalVAE(nn.Module):
     def decode(self, x: Tensor) -> Tensor:
         x = self.linear2(x)  # [N x 256*5*5]
         x = F.relu(x)
-        x = x.unflatten(-1, (128, 9, 9))  # [N x 256 x 5 x 5]
-        # x = self.tconv1(x)  # [N x 128 x 9 x 9]
-        # x = F.relu(x)
-        x = self.tconv2(x)  # [N x 64 x 17 x 17]
-        x = self.bn6(x)
-        x = F.relu(x)
-        x = self.tconv3(x)  # [N x 32 x 33 x 33]
+        x = x.unflatten(-1, (256, 5, 5))  # [N x 256 x 5 x 5]
+        x = self.tconv1(x)  # [N x 128 x 9 x 9]
         x = self.bn7(x)
         x = F.relu(x)
-        x = self.tconv4(x)  # [N x 16 x 65 x 65]
+        x = self.tconv2(x)  # [N x 64 x 17 x 17]
         x = self.bn8(x)
         x = F.relu(x)
-        x = self.tconv5(x)  # [N x 8 x 129 x 129]
+        x = self.tconv3(x)  # [N x 32 x 33 x 33]
         x = self.bn9(x)
+        x = F.relu(x)
+        x = self.tconv4(x)  # [N x 16 x 65 x 65]
+        x = self.bn10(x)
+        x = F.relu(x)
+        x = self.tconv5(x)  # [N x 8 x 129 x 129]
+        x = self.bn11(x)
         x = F.relu(x)
         x = self.tconv6(x)  # [N x C x 129 x 129]
         return x
