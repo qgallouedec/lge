32c4846945f9c4a9a48bf844258b89eab9bd2400
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2022-05-21 12:30:12 +0200

    Another encoder

--- go_explore/inverse_model.py
+++ go_explore/inverse_model.py
@@ -113,18 +113,21 @@ class ConvInverseModel(InverseModel):
         self.latent_size = latent_size
         self.obs_shape = (3, 84, 84)
         self.encoder = nn.Sequential(  # [N x 12 x 84 x 84]
-            nn.Conv2d(12, 32, kernel_size=4, stride=2),  # [N x 32 x 41 x 41]
+            nn.Conv2d(12, 32, kernel_size=3, stride=1, padding=1),  # [N x 32 x 84 x 84]
             nn.ReLU(),
             nn.BatchNorm2d(32),
-            nn.Conv2d(32, 64, kernel_size=3, stride=2),  # [N x 64 x 20 x 20]
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),  # [N x 64 x 41 x 41]
             nn.ReLU(),
             nn.BatchNorm2d(64),
-            nn.Conv2d(64, 128, kernel_size=4, stride=2),  # [N x 128 x 9 x 9]
+            nn.Conv2d(64, 128, kernel_size=3, stride=2),  # [N x 128 x 20 x 20]
             nn.ReLU(),
             nn.BatchNorm2d(128),
-            nn.Conv2d(128, 128, kernel_size=3, stride=2),  # [N x 128 x 4 x 4]
+            nn.Conv2d(128, 256, kernel_size=4, stride=2),  # [N x 256 x 9 x 9]
+            nn.ReLU(),
+            nn.BatchNorm2d(256),
+            nn.Conv2d(256, 256, kernel_size=3, stride=2),  # [N x 256 x 4 x 4]
             nn.Flatten(),
-            nn.Linear(128 * 4 * 4, 512),
+            nn.Linear(256 * 4 * 4, 512),
             nn.ReLU(),
             nn.Linear(512, latent_size),
         )
