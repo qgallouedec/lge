78ce108fc4c3b42c7d09ee27780cec9698efbbfb
Quentin GALLOUÉDEC <gallouedec.quentin@gmail.com>
2022-08-10 16:56:34 +0200

    optimize with new cov

--- experiments/optimize_lge_panda.py
+++ experiments/optimize_lge_panda.py
@@ -3,19 +3,19 @@ import numpy as np
 import optuna
 import panda_gym
 from stable_baselines3 import DDPG
-from toolbox.panda_utils import cumulative_object_coverage
+from toolbox.panda_utils import compute_coverage
 
 from lge import LatentGoExplore
 
-NUM_TIMESTEPS = 300_000
+NUM_TIMESTEPS = 200_000
 NUM_RUN = 3
 
 
 def objective(trial: optuna.Trial) -> float:
     distance_threshold = trial.suggest_categorical("distance_threshold", [0.1, 0.2, 0.5, 1.0])
-    p = trial.suggest_categorical("p", [0.001, 0.002, 0.005, 0.01, 0.02, 0.05, 0.1])
-    latent_size = trial.suggest_categorical("latent_size", [8, 16, 32, 64])
-    lighten_dist_coef = trial.suggest_categorical("lighten_dist_coef", [1, 2, 4, 8])
+    p = trial.suggest_categorical("p", [0.005, 0.01, 0.02, 0.05, 0.1])
+    latent_size = trial.suggest_categorical("latent_size", [4, 8, 16, 32])
+    lighten_dist_coef = trial.suggest_categorical("lighten_dist_coef", [0, 1, 2, 4])
 
     coverage = np.zeros((NUM_RUN, NUM_TIMESTEPS))
     for run_idx in range(NUM_RUN):
@@ -34,7 +34,7 @@ def objective(trial: optuna.Trial) -> float:
         model.explore(NUM_TIMESTEPS)
         buffer = model.archive
         observations = buffer.next_observations["observation"][: buffer.pos if not buffer.full else buffer.buffer_size]
-        coverage[run_idx] = cumulative_object_coverage(observations)
+        coverage[run_idx] = compute_coverage(observations)
 
     score = np.median(coverage[:, -1])
     return score
@@ -45,7 +45,7 @@ if __name__ == "__main__":
 
     study = optuna.create_study(
         storage="sqlite:///optuna.db",
-        study_name="lge_panda",
+        study_name="lge_panda_new_cov",
         load_if_exists=True,
         direction="maximize",
         sampler=TPESampler(n_startup_trials=25),
