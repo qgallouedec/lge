d7f9783a0b5023ffa349fa200789dd31043afbb7
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2022-12-18 20:13:16 +0100

    new explore pitfall

--- experiments/explore_lge_pitfall.py
+++ experiments/explore_lge_pitfall.py
@@ -1,24 +1,29 @@
 # pip install ale-py==0.7.4
+import time
+
 from stable_baselines3 import DQN
 from stable_baselines3.common.buffers import ReplayBuffer
 from stable_baselines3.common.callbacks import BaseCallback, CallbackList
 from wandb.integration.sb3 import WandbCallback
 
 import wandb
-from experiments.utils import MaxRewardLogger, RAMtoInfoWrapper
+from experiments.utils import AtariWrapper, MaxRewardLogger
 from lge import LatentGoExplore
 
 NUM_TIMESTEPS = 100_000
 NUM_RUN = 1
 
 env_id = "Pitfall-v4"
-module_type = "ae"
+module_type = "forward"
 latent_size = 32
 distance_threshold = 1.0
 lighten_dist_coef = 1.0
-learning_starts = 5_000
+learning_starts = 100_000
 p = 0.1
 n_envs = 4
+nb_random_exploration_steps = 200
+module_train_freq = 10_000
+module_grad_steps = 500
 
 
 class NumberRoomsLogger(BaseCallback):
@@ -39,7 +44,7 @@ class NumberRoomsLogger(BaseCallback):
 
 
 run = wandb.init(
-    name=f"lge__{env_id}__{module_type}",
+    name=f"lge__{env_id}__{module_type}__{str(time.time())}",
     project="lge",
     config=dict(
         env_id=env_id,
@@ -47,9 +52,12 @@ run = wandb.init(
         latent_size=latent_size,
         distance_threshold=distance_threshold,
         lighten_dist_coef=lighten_dist_coef,
-        leanring_starts=learning_starts,
         p=p,
         n_envs=n_envs,
+        learning_starts=learning_starts,
+        nb_random_exploration_steps=nb_random_exploration_steps,
+        module_train_freq=module_train_freq,
+        module_grad_steps=module_grad_steps,
     ),
     sync_tensorboard=True,
 )
@@ -57,24 +65,22 @@ run = wandb.init(
 model = LatentGoExplore(
     DQN,
     env_id,
-    module_type,
-    latent_size,
-    distance_threshold,
-    lighten_dist_coef,
-    p,
-    n_envs,
+    module_type=module_type,
+    latent_size=latent_size,
+    distance_threshold=distance_threshold,
+    lighten_dist_coef=lighten_dist_coef,
+    p=p,
+    n_envs=n_envs,
     learning_starts=learning_starts,
-    nb_random_exploration_steps=200,
     model_kwargs=dict(buffer_size=200_000, policy_kwargs=dict(categorical=True)),
-    wrapper_cls=RAMtoInfoWrapper,
+    wrapper_cls=AtariWrapper,
+    nb_random_exploration_steps=nb_random_exploration_steps,
+    module_train_freq=module_train_freq,
+    module_grad_steps=module_grad_steps,
     tensorboard_log=f"runs/{run.id}",
     verbose=1,
 )
-wandb_callback = WandbCallback(
-    gradient_save_freq=100,
-    model_save_path=f"models/{run.id}",
-    verbose=2,
-)
 
-model.explore(NUM_TIMESTEPS, CallbackList([NumberRoomsLogger(), MaxRewardLogger(), wandb_callback]))
+
+model.explore(NUM_TIMESTEPS, CallbackList([NumberRoomsLogger(), MaxRewardLogger()]))
 run.finish()
