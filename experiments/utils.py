70084e4ddd5f760ff1b97eb7e28d3f64f105fe7e
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2022-12-18 20:12:17 +0100

    New atari wrapper

--- experiments/utils.py
+++ experiments/utils.py
@@ -1,9 +1,43 @@
+import gym
 import numpy as np
-from stable_baselines3.common.atari_wrappers import AtariWrapper
+from stable_baselines3.common.atari_wrappers import EpisodicLifeEnv, FireResetEnv, MaxAndSkipEnv, WarpFrame
 from stable_baselines3.common.callbacks import BaseCallback
 
 
-class RAMtoInfoWrapper(AtariWrapper):
+class AtariWrapper(gym.Wrapper):
+    """
+    Atari 2600 preprocessings
+
+    Specifically:
+
+    * NoopReset: obtain initial state by taking random number of no-ops on reset.
+    * Frame skipping: 4 by default
+    * Max-pooling: most recent two observations
+    * Termination signal when a life is lost.
+    * Resize to a square image: 84x84 by default
+    * Grayscale observation
+    * Clip reward to {-1, 0, 1}
+
+    :param env: gym environment
+    :param noop_max: max number of no-ops
+    :param frame_skip: the frequency at which the agent experiences the game.
+    :param screen_size: resize Atari frame
+    :param terminal_on_life_loss: if True, then step() returns done=True whenever a life is lost.
+    :param clip_reward: If True (default), the reward is clip to {-1, 0, 1} depending on its sign.
+    """
+
+    def __init__(
+        self,
+        env: gym.Env,
+        frame_skip: int = 4,
+        screen_size: int = 84,
+    ):
+        env = MaxAndSkipEnv(env, skip=frame_skip)
+        env = EpisodicLifeEnv(env)
+        env = FireResetEnv(env)
+        env = WarpFrame(env, width=screen_size, height=screen_size)
+        super().__init__(env)
+
     def step(self, action):
         obs, reward, done, info = super().step(action)
         info["ram"] = self.unwrapped.ale.getRAM()
@@ -25,5 +59,5 @@ class MaxRewardLogger(BaseCallback):
 
         rewards = [info[env_idx]["env_reward"] for info in infos for env_idx in range(buffer.n_envs)]
         self.max_reward = max(np.max(rewards), self.max_reward)
-        self.logger.record("env/max env eward", self.max_reward)
+        self.logger.record("env/max_env_eward", self.max_reward)
         return True
